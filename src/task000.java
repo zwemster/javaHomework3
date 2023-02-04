1d1eba417178eee9bf34e5ffe9c9fbfef9c2b76b
maxim_stoma <maksim.stoma@mail.ru>
2023-02-04 23:59:45 +0300

    task000 (palindrome task) solved

--- src/task000.java
+++ src/task000.java
@@ -1,2 +1,23 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
 public class task000 {
+    public static void main(String[] args){
+        Deque<Integer> deque = new ArrayDeque<>(Arrays.asList(1,2,3,4,5,6));
+//        Deque<Integer> deque = new ArrayDeque<>(Arrays.asList(1,2,3,3,2,1));
+        boolean palindrome = checkOn(deque);
+        if (palindrome) {System.out.println("Последовательность чисел является палиндромом.");
+        } else {System.out.println("Последовательность чисел не является палиндромом.");
+        }
+    }
+
+    public static boolean checkOn(Deque<Integer> deque){
+        while (deque.size() > 1){
+            if (deque.pollFirst().equals(deque.pollLast())){
+                return true;
+            }
+        }
+        return false;
+    }
 }
